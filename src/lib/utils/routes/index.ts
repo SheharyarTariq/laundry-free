f143f96252b1d704487c14dec4c1b66f4debcd8c
SheharyarTariq <sheharyartariqbutt@gmail.com>
2025-08-24 01:50:38 +0500

    post codes

--- src/lib/utils/routes/index.ts
+++ src/lib/utils/routes/index.ts
@@ -15,10 +15,10 @@ export const routes = {
   api: {
     login: "/api/auth/login",
     postArea: "/areas",
+    postPostcode: "/postcodes",
     deleteArea:(id: string) => `/areas/${id}`,
     getAreas:(id: string) => `/areas?page=${id}`,
     getPostcodes:(id: string) => `/areas/${id}/postcodes`,
-    postPostcode: "/postcodes",
     deletePostcode:(id: string) => `/postcodes/${id}`,
     activePostcode:(id: string) => `/postcodes/${id}/mark-as-active`,
     inactivePostcode:(id: string) => `/postcodes/${id}/mark-as-in-active`
